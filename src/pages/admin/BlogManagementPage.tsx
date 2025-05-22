7cc0b4fc19b5cc2188c7265b123f918ac99e3728
saffire-tech <saffiretech01@gmail.com>
2025-05-22 00:04:00 +0000

    Fix BlogManagementPage import path

---
+++ src/pages/admin/BlogManagementPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogManagementPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Blog Management</h1>
+      {/* Blog management content will go here */}
+    </div>
+  );
+};
+
+export default BlogManagementPage;
